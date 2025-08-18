2629fa9536279140fc694fd683982d1d6c0d1eb9
pnb-rae <ryanwaweru475@gmail.com>
2025-08-18 13:31:45 +0300

    new features

--- advanced_features_and_security/users/admin.py
+++ advanced_features_and_security/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active")}
+        ),
+    )
+
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
